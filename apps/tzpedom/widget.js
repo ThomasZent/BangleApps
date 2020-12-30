f3f3b2a8ce2964c0313154fbbd498514e2144123
Thomas Zentmyer <thomas.zent@gmail.com>
2020-12-29 22:59:01 -0800

    Fix syntax errors

--- apps/tzpedom/widget.js
+++ apps/tzpedom/widget.js
@@ -17,9 +17,9 @@
       'maxTime' : 1000,
       'minTime' : 400,
       'threshold' : 10,
-      'actres' : 10000,
-      'sens' : 100,
-      'goal' : 10000
+      'actres' : 11000,
+      'sens' : 90,
+      'goal' : 10000,
       'stride' : 126
     };
 
@@ -87,7 +87,7 @@
   
   Bangle.on('step', (up) => {
     let timeDiff = Date.now() - lastStep;
-    let lastStep = Date.now();
+    lastStep = Date.now();
     
     if (timeDiff >= setting('maxTime')) return;
     if (timeDiff <= setting('minTime')) return;
